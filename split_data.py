781861cd25f7e047ec938c6bfdccc64cd4e81400
Janek <jankusiowski3@gmail.com>
2026-03-03 15:07:06 +0100

    data split for both models, 45k -> 3k -> 4k test

---
+++ split_data.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("data_fixed.csv")
+data = data.drop_duplicates()
+
+
+df_35, df_temp = train_test_split(
+    data,
+    train_size=45000,
+    stratify=data["is_phish"],
+    random_state=42
+)
+df_10, df_test = train_test_split(
+    df_temp,
+    train_size=3000,
+    stratify=df_temp["is_phish"],
+    random_state=42
+)
+
+df_35.to_csv("data_35k.csv", index=False)
+df_10.to_csv("data_10k.csv", index=False)
+df_test.to_csv("data_test.csv", index=False)
